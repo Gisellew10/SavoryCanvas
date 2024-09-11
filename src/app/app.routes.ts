e24bd961995a2624aaed3a65a924b4b713790812
Giselle Wang <94551327+Gisellew10@users.noreply.github.com>
2024-09-10 21:26:31 -0400

    feat: set up dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'dashboard', 
+        component: DashboardComponent 
+    },
+    { 
+        path: '', 
+        redirectTo: 'dashboard', 
+        pathMatch: 'full' 
+    },
+    {
+        path:'recipe/:id',
+        component: RecipeComponent
+    }
+];
